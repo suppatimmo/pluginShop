d9d7b2c55b04a06e6949374b3dc8b6284bc27384
suppatimmo <boroszek717@gmail.com>
2020-09-05 14:24:19 +0200

    Initial commit

---
+++ src/main/java/com/pluginShop/pluginShop/PluginShopApplication.java
@@ -0,0 +1,13 @@
+package com.pluginShop.pluginShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluginShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pluginShop/pluginShop/PluginShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pluginShop.pluginShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
