357f741c116082cedbb6ed6d049fac79b8dd34ab
suppatimmo <boroszek717@gmail.com>
2020-09-06 13:23:36 +0200

    update gitignore

--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -2,10 +2,11 @@ package repository;
 
 import model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Integer> {
+public interface UserRepository extends CrudRepository<User, Integer> {
     User getUserById(Integer id);
 }
